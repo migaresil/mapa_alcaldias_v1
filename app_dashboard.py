5036a55bcf76c3122959803bc7c742824741b0ef
migaresil <migaresil@gmail.com>
2025-10-24 18:13:29 -0600

    Fixes en app

--- app_dashboard.py
+++ app_dashboard.py
@@ -1,4 +1,4 @@
-import streamlit as st 
+import streamlit as st
 from streamlit_folium import st_folium
 from data_loader import load_data
 from map_utils import load_geojson, render_folium_map
@@ -6,12 +6,10 @@ from map_utils import load_geojson, render_folium_map
 st.set_page_config(page_title="Dashboard CDMX", layout="wide")
 st.title("🚨 Dashboard de Incidentes Delictivos – CDMX")
 
-
 # === 1. Carga de datos y GeoJSON ===
 url_geojson = "https://datos.cdmx.gob.mx/dataset/alcaldias/resource/8648431b-4f34-4f1a-a4b1-19142f944300/download/limite-de-las-alcaldias.json"
 delegaciones = load_geojson(url_geojson)
 df = load_data("df_streamlit.csv")
-
 
 # === 2. Controles de interfaz ===
 st.sidebar.header("⚙️ Configuración del mapa")
